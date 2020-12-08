aa77348b35f3e268da1290e79eaa45dc6cab0817
Alex Kolva <aleksandr.kolva@gmail.com>
2020-12-08 02:06:48 +0200

    users post handler

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var stringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
-
-// Testing
-// @TODO: delete this
-var _data = require('./lib/data');
-_data.delete('test', 'newFile', function (err) {
-	console.log('This was the error', err);
-});
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 // Instantiate the HTTP server
 var httpServer = http.createServer(function(req, res) {
@@ -72,7 +67,7 @@ var unifiedServer = function (req, res) {
 	req.on('end', function() {
 		buffer += decoder.end();
 
-		// Choose the handler this req thould go to.
+		// Choose the handler this req should go to.
 		// If one is not found use not found handler
 		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
@@ -82,7 +77,7 @@ var unifiedServer = function (req, res) {
 			'queryStringObject': queryStringObject,
 			'method': method,
 			'headers': headers,
-			'payload': buffer
+			'payload': helpers.parseJsonToObject(buffer)
 		};
 
 		// Route the request to the handler specified in the router
@@ -109,20 +104,8 @@ var unifiedServer = function (req, res) {
 	});
 };
 
-// Define handlers
-var handlers = {};
-
-// Ping handler
-handlers.ping = function (data, callback) {
-	callback(200);
-};
-
-// Define not found handler
-handlers.notFound = function (data, callback) {
-	callback(404);
-};
-
 // Define a request router
 var router = {
-	'ping': handlers.ping
+	'ping': handlers.ping,
+	'users': handlers.users
 };
